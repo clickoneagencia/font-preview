e5144940c65f5cbe9ffa20225e141207753d3386
Alejandro E. Rendon <alejandro@rendon.co>
2022-07-09 22:36:11 -0500

    ✨ Add fonts and inputs to manage texts

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(() => {
   $(".ui.dropdown").dropdown();
 
+  // Change input content
   $("#input").on("input", function () {
-    // Print entered value in a div box
-    // $("#result").text($(this).val());
-    console.log($(this).val())
+    const newText = $(this).val();
+
+    $(".preview-text").text(newText);
+  });
+
+  // Change font size
+  $(".item-menu-font-size").click(function () {
+    const newFontSizeValue = $(this).data("value");
+
+    $(".preview-text").css("font-size", `${newFontSizeValue}px`);
   });
 });
